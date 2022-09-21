d42ae6e9034965d035db32206b1b78a2a7412acd
einnarth <113421210+einnarth@users.noreply.github.com>
2022-09-21 18:01:04 +0200

    Update Boolean.cs

--- Open-Lab-01.05/Boolean.cs
+++ Open-Lab-01.05/Boolean.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._05
 {
@@ -6,7 +6,7 @@ namespace Open_Lab_01._05
     {
         public bool Reverse(bool value)
         {
-            throw new NotImplementedException();
+            return !value;
         }
     }
 }
